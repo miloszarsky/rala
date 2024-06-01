389e626d49a1cf389ac271ccb98cacae61977111
Milos Zarsky <zarsky@master.cz>
2024-06-01 16:29:27 +0200

    functions and pushover

--- log_analyzer/log_analyzer.py
+++ log_analyzer/log_analyzer.py
@@ -6,10 +6,12 @@ from itertools import takewhile, dropwhile
 import smtplib
 from email.mime.text import MIMEText
 import paramiko
+import http.client, urllib
 
 r = redis.Redis(host='rala-redis', port=6379, decode_responses=True)
 
 # Mail Configuration
+mailing = os.getenv('MAILING')
 port = os.getenv('MAIL_PORT')
 smtp_server = os.getenv('MAIL_SERVER')
 login = os.getenv('MAIL_LOGIN')
@@ -22,11 +24,49 @@ ssh_host = os.getenv('SSH_HOST')
 ssh_port = os.getenv('SSH_PORT')
 ssh_username = os.getenv('SSH_USERNAME')
 ssh_password = os.getenv('SSH_PASSWORD')
-ssh_command1 = os.getenv('SSH_CMD1')
-ssh_command2 = os.getenv('SSH_CMD2')
+ssh4_cmd1 = os.getenv('SSH4_CMD1')
+ssh4_cmd2 = os.getenv('SSH4_CMD2')
+ssh6_cmd1 = os.getenv('SSH6_CMD1')
+ssh6_cmd2 = os.getenv('SSH6_CMD2')
+
+# PUSHOVER Configuration
+pushover = os.getenv('PUSHOVER')
+pushover_user = os.getenv('PUSHOVER_USER')
+pushover_token = os.getenv('PUSHOVER_TOKEN')
 
 # ENV
 ip_count_threshold = os.getenv('IP_COUNT_THRESHOLD')
+
+#function actions
+def mailer (addrs):
+    message = MIMEText(str(addrs), "plain")
+    message["Subject"] = "Rala"
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.starttls()
+        server.login(login, password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+
+def ssh4 (addrs4):
+    client = paramiko.client.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ssh_host, username=ssh_username, password=ssh_password, port=ssh_port)
+    _stdin, _stdout,_stderr = client.exec_command(str(ssh4_cmd1) + str(addrs4) + str(ssh4_cmd2))
+    client.close()
+
+def ssh6 (addrs6):
+    client = paramiko.client.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ssh_host, username=ssh_username, password=ssh_password, port=ssh_port)
+    _stdin, _stdout,_stderr = client.exec_command(str(ssh6_cmd1) + str(addrs6) + str(ssh6_cmd2))
+    client.close()
+
+def pushoversender(address):
+    data={"token": pushover_token,"user": pushover_user,"message": str(address),}
+    conn = http.client.HTTPSConnection("api.pushover.net:443")
+    conn.request("POST", "/1/messages.json", urllib.parse.urlencode(data), { "Content-type": "application/x-www-form-urlencoded" })
+    conn.getresponse()
 
 while True:
     ips4=[]
@@ -42,23 +82,11 @@ while True:
         for addrs4, counter in higher4.items():
             #whatever you want to do with ip in key
             print(addrs4)
-            ###
-            #email result
-            #message = MIMEText(str(addrs4), "plain")
-            #message["Subject"] = "Rala"
-            #message["From"] = sender_email
-            #message["To"] = receiver_email
-            #with smtplib.SMTP(smtp_server, port) as server:
-            #    server.starttls()
-            #    server.login(login, password)
-            #    server.sendmail(sender_email, receiver_email, message.as_string())
-            ###
-            #run action over ssh
-            client = paramiko.client.SSHClient()
-            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            client.connect(ssh_host, username=ssh_username, password=ssh_password, port=ssh_port)
-            _stdin, _stdout,_stderr = client.exec_command(str(ssh_command1) + str(addrs4) + str(ssh_command2))
-            client.close()
+            if mailing == "true":
+                mailer(addrs4)
+            if pushover == "true":
+                pushoversender(addrs4)
+            ssh4(addrs4)
     for ip6 in Counter(r.keys(pattern='ip6-*')):
         #print(r.get(ip6))
         ips6.append(r.get(ip6))
@@ -70,21 +98,9 @@ while True:
         for addrs6, counter in higher6.items():
             #whatever you want to do with ip in key
             print(addrs6)
-            ###
-            #email result
-            #message = MIMEText(str(addrs6), "plain")
-            #message["Subject"] = "Rala"
-            #message["From"] = sender_email
-            #message["To"] = receiver_email
-            #with smtplib.SMTP(smtp_server, port) as server:
-            #    server.starttls()
-            #    server.login(login, password)
-            #    server.sendmail(sender_email, receiver_email, message.as_string())
-            ###
-            #run action over ssh
-            client = paramiko.client.SSHClient()
-            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            client.connect(ssh_host, username=ssh_username, password=ssh_password, port=ssh_port)
-            _stdin, _stdout,_stderr = client.exec_command(str(ssh_command1) + str(addrs6) + str(ssh_command2))
-            client.close()
+            if mailing == "true":
+                mailer(addrs6)
+            if pushover == "true":
+                pushoversender(addrs6)
+            ssh6(addrs6)
     time.sleep(5)
